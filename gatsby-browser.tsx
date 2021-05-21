99ff44a9f54e74369e1f9c5bed2b7a463a16beae
MedElBoudali <med.sneaky@gmail.com>
2021-05-21 23:05:32 +0000

    🎨 removed the comment

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import Layout from "./src/components/layout/Layout";
+import Layout from "./src/components/layout";
 import { WrapPageElementBrowserArgs } from "gatsby";
-
-// interface wrapPageElementProps {
-//   element: React.ReactNode;
-//   props: Object;
-// }
 
 export const wrapPageElement = ({
   element,
