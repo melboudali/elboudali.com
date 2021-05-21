9cad661c886e7368ec69ad10a30328ad4f0e89b8
MedElBoudali <med.sneaky@gmail.com>
2021-05-21 20:55:42 +0000

    🎨 added image and modified about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 import PropTypes from "prop-types";
 
 interface indexProps {}
 
 const index = ({}: indexProps) => {
-  return <div>This is index Component/Page</div>;
+  console.log("mounted");
+  return (
+    <div>
+      This is index Component/Page
+      <StaticImage
+        src="../assets/images/FB_IMG_1438101555768.jpg"
+        alt=""
+        placeholder="blurred"
+        layout="fixed"
+        width={200}
+        height={200}
+      />
+    </div>
+  );
 };
 
 index.propTypes = {};
