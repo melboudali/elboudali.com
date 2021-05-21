617aaf3d1ab75abb4565a128cd89ddbf4947f131
MedElBoudali <med.sneaky@gmail.com>
2021-05-21 23:34:58 +0000

    💄 moved themes

---
+++ src/themes/index.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  navbarBackground: "var(--primaryBackgroundColorLight)",
+  bodyBackground: "var(--secondaryBackgroundColorLight)",
+  navColor: "var(--black)",
+  titleColor: "var(--primaryColorLight)",
+  iconsColor: "var(--primaryColorLight)",
+  textColor: "var(--black)",
+  linearRightColor: "var(--linearRightColorLight)",
+  linearLeftColor: "var(--linearLeftColorLight)",
+};
+
+export const darkTheme: DefaultTheme = {
+  navbarBackground: "var(--primaryBackgroundColorDark)",
+  bodyBackground: "var(--secondaryBackgroundColorDark)",
+  navColor: "var(--white)",
+  titleColor: "var(--primaryColorDark)",
+  iconsColor: "var(--secondaryColor)",
+  textColor: "var(--primaryColorDark)",
+  linearRightColor: "var(--linearRightColorDark)",
+  linearLeftColor: "var(--linearLeftColorDark)",
+};
