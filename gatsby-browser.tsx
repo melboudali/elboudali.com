264609162e3658a27c6c37f7724552c80963acfd
MedElBoudali <med.sneaky@gmail.com>
2021-05-24 23:24:32 +0000

    🐛 trying to fix a bug

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Layout from "./src/components/layout";
-import { WrapPageElementBrowserArgs } from "gatsby";
 
-export const wrapPageElement = ({
-  element,
-  props,
-}: WrapPageElementBrowserArgs) => <Layout {...props}>{element}</Layout>;
+interface wrapPageElementInterface {
+  element: React.ReactNode;
+  props: Object;
+}
+
+export const wrapPageElement = ({ element, props }: wrapPageElementInterface) => <Layout {...props}>{element}</Layout>;
