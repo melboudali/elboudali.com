ac0e55acbad1692161be897e4fb1a17d829cfe8f
MedElBoudali <med.sneaky@gmail.com>
2021-05-24 21:41:49 +0000

    🎨 added  flags: { PRESERVE_WEBPACK_CACHE: true }

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,10 +7,6 @@ export default {
     siteUrl: "https://elboudali.com",
     twitter: "@moelboudali",
   },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-react-helmet",
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`
-  ],
+  flags: { PRESERVE_WEBPACK_CACHE: true },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet", `gatsby-plugin-image`, `gatsby-plugin-sharp`],
 };
