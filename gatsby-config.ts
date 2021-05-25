8f33f9d3cd8366e49d755c95c202146830a2770a
MedElBoudali <med.sneaky@gmail.com>
2021-05-25 23:49:25 +0000

    🔧 added gatsby-source-filesystem with download folder

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,5 +9,17 @@ export default {
     siteUrl: config.siteUrl,
     twitter: config.socialLinks.twitterId,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet", `gatsby-plugin-image`, `gatsby-plugin-sharp`],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `download`,
+        path: `${__dirname}/src/static/download/`,
+      },
+    },
+  ],
 };
