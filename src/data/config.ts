712dad1ae4eb07a39b4430a620f3841835421b1c
MedElBoudali <med.sneaky@gmail.com>
2021-05-25 21:57:46 +0000

    🔧 added config file

---
+++ src/data/config.ts
@@ -0,0 +1,21 @@
+const config = {
+  title: "< el boudali />",
+  fullName: "mohamed el boudali",
+  desciption:
+    "I'm a self-taught Full Stack Developer, proficient in Front End, with 2+ years of hands-on experience designing, developing, and implementing applications and solutions using a range of technologies.",
+  role: "front end developer",
+  location: "morocco",
+  socialLinks: {
+    email: "mailto:contact@elboudali.com",
+    Linkedin: "https://www.linkedin.com/in/elboudali",
+    github: "https://github.com/melboudali",
+    twitter: "https://twitter.com/moelboudali",
+    freecodecamp: "https://www.freecodecamp.org/elboudali",
+    hackerrank: "https://www.hackerrank.com/med_sneaky",
+    reddit: "https://www.reddit.com/user/mflyback/",
+    twitterId: "@moelboudali",
+  },
+  siteUrl: "https://elboudali.com",
+};
+
+export default config;
