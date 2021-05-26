d7f1e10da5ad4134689b597d53be451dcf26ccf2
MedElBoudali <med.sneaky@gmail.com>
2021-05-26 23:58:13 +0000

    💄 added SchoolDegree component

---
+++ src/components/about/SchoolDegree.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+interface SchoolDegreeProps {}
+
+const SchoolDegree = ({}: SchoolDegreeProps) => {
+  return <div>This is SchoolDegree Component/Page</div>;
+};
+
+SchoolDegree.propTypes = {};
+
+export default SchoolDegree;
