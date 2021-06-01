d525720bd4225602f320540cac770404800c9582
MedElBoudali <med.sneaky@gmail.com>
2021-06-01 22:58:37 +0000

    🎨 added InputGroup component

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import PageTitle from "../components/common/PageTitle";
+import useForm from "../hook/useForm";
 
 const ContactWrapper = styled.section`
   padding: 0 193px;
@@ -17,20 +18,36 @@ const Input = styled.input``;
 
 interface contactProps {}
 
+interface InputGroupProps {
+  values: { name: string; email: string; message: string };
+  updateValue: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: "name" | "email" | "message";
+}
+
 const contact = ({}: contactProps) => {
+  const { values, updateValue, clearValues } = useForm({
+    name: "",
+    email: "",
+    message: "",
+  });
+
   return (
     <ContactWrapper>
       <PageTitle>Send an email to Mohamed EL BOUDALI</PageTitle>
-      <FormWrapper></FormWrapper>
+      <FormWrapper>
+        <InputGroup values={values} updateValue={updateValue} name="name" />
+        <InputGroup values={values} updateValue={updateValue} name="email" />
+        <InputGroup values={values} updateValue={updateValue} name="message" />
+      </FormWrapper>
     </ContactWrapper>
   );
 };
 
-const InputGroup = () => {
+const InputGroup = ({ values, updateValue, name }: InputGroupProps) => {
   return (
     <InputGroupWrapper>
-      <InoutLabel>your name</InoutLabel>
-      <Input type="text"></Input>
+      <InoutLabel>your {name}</InoutLabel>
+      <Input name={name} type={name === "email" ? "email" : "text"} value={values.name} onChange={updateValue} />
     </InputGroupWrapper>
   );
 };
