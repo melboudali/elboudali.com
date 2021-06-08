c2a36dc6eea1251dd78caebe85f489fee37cb551
MedElBoudali <med.sneaky@gmail.com>
2021-06-08 22:45:57 +0000

    ✨ added ItemsCount and ItemsCountNumber

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -4,6 +4,9 @@ import PropTypes from "prop-types";
 
 const PageTitleWrapper = styled.h1`
   --fontSize: 1.8rem;
+  display: flex;
+  align-items: flex-start;
+  gap: 10px;
   font-size: var(--fontSize);
   font-weight: 700;
   letter-spacing: -0.04em;
@@ -14,12 +17,30 @@ const PageTitleWrapper = styled.h1`
   }
 `;
 
+const ItemsCount = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.6875rem;
+  border-radius: 5px;
+  padding: 2px 4px;
+  letter-spacing: normal;
+  background: ${({ theme }) => theme.emailBorder};
+  color: ${({ theme }) => theme.balackAndWhite};
+`;
+
 interface PageTitleProps {
   children: string;
+  ItemsCountNumber?: number;
 }
 
-const PageTitle = ({ children }: PageTitleProps) => <PageTitleWrapper>{children}</PageTitleWrapper>;
+const PageTitle = ({ children, ItemsCountNumber = 0 }: PageTitleProps) => (
+  <PageTitleWrapper>
+    {children}
+    {!!ItemsCountNumber && <ItemsCount>{ItemsCountNumber}</ItemsCount>}
+  </PageTitleWrapper>
+);
 
-PageTitle.propTypes = { children: PropTypes.string.isRequired };
+PageTitle.propTypes = { children: PropTypes.string.isRequired, ItemsCountNumber: PropTypes.number };
 
 export default PageTitle;
