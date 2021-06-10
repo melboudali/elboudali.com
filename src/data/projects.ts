687c8d8eb8450e3f9bb84b7a32fa42184344b693
MedElBoudali <med.sneaky@gmail.com>
2021-06-10 22:54:17 +0000

    🔨 modified projects array

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,30 +1,11 @@
 const projects = [
   {
-    "Instagram-Clone": {
-      id: "Instagram-Clone",
-      name: "📸 Instagram Clone",
-      srcURL: { url: "https://github.com/melboudali/Instagram-Clone" },
-      demoURL: { url: "http://instagram.elboudali.com/" },
-      cover: { srcPath: "projects/Instagram-Clone/cover.png" },
-      startDate: "2020-12-21",
-      endDate: "2021-03-28",
-      summary: [
-        "Instagram Clone is a full-stack application built with react, styled-components, typescript, graphql, cloudinary, typeorm, express, redis, PostgreSQL",
-      ],
-      tags: [
-        { name: "javaScript" },
-        { name: "TypeScript" },
-        { name: "React" },
-        { name: "GraphQl" },
-        { name: "Typeorm" },
-        { name: "Express" },
-        { name: "Redis" },
-        { name: "PostgreSQL" },
-      ],
-      gitHubRepo: {
-        owner: "melboudali",
-        repo: "Instagram-Clone",
-      },
-    },
+    name: "Instagram-Clone",
+    title: "📸 Instagram Clone",
+    cover: "projects/Instagram-Clone/cover.png",
+    startDate: "2020-12-21",
+    endDate: "2021-03-28",
   },
 ];
+
+export default projects;
