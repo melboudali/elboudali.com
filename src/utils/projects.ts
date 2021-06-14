0b908e11977659a8ced0578fb1b90cf46a033a82
MedElBoudali <med.sneaky@gmail.com>
2021-06-14 21:42:54 +0000

    ✨ added sortProjects func

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,11 +1,23 @@
 import { Repo } from "../../gatsby-graphql";
 
-export const getAllReposStars = (
-  repos: Pick<Repo, "name" | "created_at" | "description" | "forks_count" | "html_url" | "stargazers_count" | "language" | "homepage">[]
-) => {
+type reposType = (Pick<
+  Repo,
+  "id" | "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"
+> & {
+  fromNow: Repo["created_at"];
+})[];
+
+export const getAllReposStars = (repos: reposType) => {
   let sum = 0;
   repos.map(repo => (sum += repo.stargazers_count!));
   return sum;
+};
+
+export const sortProjects = (repos: reposType, sortBy: string) => {
+  if (sortBy === "stargazers_count") {
+    return repos.sort((a, b) => b.stargazers_count! - a.stargazers_count!);
+  }
+  return repos.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
 };
 
 export const getSelectedProjects = () => {};
