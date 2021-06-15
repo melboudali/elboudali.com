f10ab7e73f8d1dd448126d23d81cda43997ebd97
MedElBoudali <med.sneaky@gmail.com>
2021-06-15 19:58:05 +0000

    🏷️ + types files.

---
+++ src/types/projects.d.ts
@@ -0,0 +1,32 @@
+import { Repo } from "../../gatsby-graphql";
+
+export interface selectedProjectType {
+  project_name: string;
+  project_title: string;
+  project_cover: string;
+  project_startDate: string;
+  project_endDate: string;
+  project_topis: string[];
+  project_figmaLink: string;
+}
+
+export type repoType = Pick<
+  Repo,
+  "id" | "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"
+> & {
+  fromNow: Repo["created_at"];
+};
+
+export type cardRepoType = Pick<
+  Repo,
+  "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"
+> & {
+  fromNow: Repo["created_at"];
+};
+
+export type fullCardRepoType = Pick<
+  Repo,
+  "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"
+> & {
+  fromNow: Repo["created_at"];
+} & selectedProjectType;
---
+++ src/types/styled-components.d.ts
@@ -0,0 +1,25 @@
+// import original module declarations
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme {
+    navbarBackground: string;
+    bodyBackground: string;
+    navColor: string;
+    titleColor: string;
+    textColor: string;
+    linearRightColor: string;
+    linearLeftColor: string;
+    balackAndWhite: string;
+    modalBackground: sring;
+    modalShadow: string;
+    labelColor: string;
+    inputBackground: string;
+    buttonBackground: string;
+    emailBackground: string;
+    emailBorder: string;
+    selectBackground: string;
+    starsColor: string;
+  }
+}
