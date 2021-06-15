65db0925bb425d7a66cae1af5dd485915c438154
MedElBoudali <med.sneaky@gmail.com>
2021-06-15 19:51:24 +0000

    ✨ + getProjectsNames, filterProjects and getSelectedProject functions.

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,25 +1,39 @@
-import { Repo } from "../../gatsby-graphql";
+import { selectedProjectType, repoType, cardRepoType, fullCardRepoType } from "../types/projects";
 
-type reposType = (Pick<
-  Repo,
-  "id" | "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"
-> & {
-  fromNow: Repo["created_at"];
-})[];
+const getProjectsNames = (selectedProjects: selectedProjectType[]) => selectedProjects.map(project => project.project_name);
 
-export const getAllReposStars = (repos: reposType) => {
+export const getAllReposStars = (repos: repoType[]) => {
   let sum = 0;
   repos.map(repo => (sum += repo.stargazers_count!));
   return sum;
 };
 
-export const sortProjects = (repos: reposType, sortBy: string) => {
+export const sortProjects = (repos: repoType[], sortBy: string) => {
   if (sortBy === "stargazers_count") {
     return repos.sort((a, b) => b.stargazers_count! - a.stargazers_count!);
   }
   return repos.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
 };
 
-export const getSelectedProjects = () => {};
+export const filterProjects = (repos: repoType[], selectedProjects: selectedProjectType[]) => {
+  return repos.filter(repo => getProjectsNames(selectedProjects).includes(repo.name!));
+};
 
-export const getSelectedProject = () => {};
+export const getSelectedProject = (repo: cardRepoType, selectedProjects: selectedProjectType[]): fullCardRepoType => {
+  let projectPlaceHolder: selectedProjectType = {
+    project_name: "",
+    project_title: "",
+    project_cover: "",
+    project_startDate: "",
+    project_endDate: "",
+    project_topis: [""],
+    project_figmaLink: "",
+  };
+  let data = { ...repo, ...projectPlaceHolder };
+  selectedProjects.map(project => {
+    if (project.project_name === repo.name) {
+      data = { ...repo, ...project };
+    }
+  });
+  return data;
+};
