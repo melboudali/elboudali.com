8e06127fdbe717af296995d0c29a145ceecf2f14
MedElBoudali <med.sneaky@gmail.com>
2021-06-15 19:48:47 +0000

    💄 + dark\light themes.

---
+++ src/themes/dark.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  navbarBackground: "var(--primaryBackgroundColorDark)",
+  bodyBackground: "var(--secondaryBackgroundColorDark)",
+  navColor: "var(--white)",
+  titleColor: "var(--primaryColorDark)",
+  textColor: "var(--primaryColorDark)",
+  linearRightColor: "var(--linearRightColorDark)",
+  linearLeftColor: "var(--linearLeftColorDark)",
+  balackAndWhite: "var(--white)",
+  modalBackground: "var(--modalDarkBackground)",
+  modalShadow: "var(--modalDarkShadow)",
+  labelColor: "var(--labelColor)",
+  inputBackground: "var(--inputDarkBackground)",
+  buttonBackground: "var(--buttonBackgroundDark)",
+  emailBackground: "var(--emailDarkBackground)",
+  emailBorder: "var(--emailDarkBorder)",
+  selectBackground: "var(--selectDarkBackground)",
+  starsColor: "var(--starsDarkColor);",
+};
---
+++ src/themes/light.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  navbarBackground: "var(--primaryBackgroundColorLight)",
+  bodyBackground: "var(--secondaryBackgroundColorLight)",
+  navColor: "var(--black)",
+  titleColor: "var(--primaryColorLight)",
+  textColor: "var(--black)",
+  linearRightColor: "var(--linearRightColorLight)",
+  linearLeftColor: "var(--linearLeftColorLight)",
+  balackAndWhite: "var(--black)",
+  modalBackground: "var(--modalLightBackground)",
+  modalShadow: "var(--modalLightShadow)",
+  labelColor: "var(--labelColor)",
+  inputBackground: "var(--inputLightBackground)",
+  buttonBackground: "var(--buttonBackgroundLight)",
+  emailBackground: "var(--emailLightBackground)",
+  emailBorder: "var(--emailLightBorder)",
+  selectBackground: "var(--selectLightBackground)",
+  starsColor: "var(--starsLightColor);",
+};
