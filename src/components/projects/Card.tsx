0c13371dd0c4104f1cb3cbe09971bf140410d620
MedElBoudali <med.sneaky@gmail.com>
2021-06-15 19:38:26 +0000

    💩 + code spaghetti.

--- src/components/projects/Card.tsx
+++ src/components/projects/Card.tsx
@@ -1,17 +1,36 @@
 import React from "react";
-import { Repo } from "../../../gatsby-graphql";
+import { cardRepoType, fullCardRepoType } from "../../types/projects";
 import PropTypes from "prop-types";
+import { getSelectedProject } from "../../utils/projects";
+import selectedProjects from "../../data/projects";
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image";
+import { graphql, useStaticQuery } from "gatsby";
+import { File } from "../../../gatsby-graphql";
 
 interface CardProps {
-  repo: Pick<Repo, "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"> & {
-    fromNow: Repo["created_at"];
-  };
+  repo: fullCardRepoType;
+}
+
+interface FileInt {
+  file: File;
 }
 
 const Card = ({ repo }: CardProps) => {
+  const { file }: FileInt = useStaticQuery(graphql`
+    query {
+      file: file(relativePath: { eq: "Instagram-Clone/cover.png" }) {
+        childrenImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  `);
+
   return (
     <div>
       {repo.name} - {repo.fromNow} - {repo.stargazers_count}
+      <GatsbyImage image={getImage(file.childImageSharp?.gatsbyImageData)!} alt="EL BOUDALI" className="my_image" />
+      {/* <StaticImage src={file.} alt="EL BOUDALI" className="my_image" /> */}
     </div>
   );
 };
