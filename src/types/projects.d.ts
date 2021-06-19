9702f2c939165aa159b80fc21a3d9e52b6805125
MedElBoudali <med.sneaky@gmail.com>
2021-06-19 21:16:12 +0000

    🏷️ -+ types.

--- src/types/projects.d.ts
+++ src/types/projects.d.ts
@@ -4,32 +4,18 @@ export interface selectedProjectType {
   project_name: string;
   project_title: string;
   project_cover: string;
-  project_startDate: string;
-  project_endDate: string;
   project_topis: string[];
   project_figmaLink: string;
 }
 
-export type repoType = Pick<
-  Repo,
-  "id" | "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"
-> & {
-  fromNow: Repo["created_at"];
-};
-
 export type cardRepoType = Pick<
   Repo,
-  "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"
-> & {
-  fromNow: Repo["created_at"];
-};
+  "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at" | "pushed_at"
+>;
 
-export type fullCardRepoType = Pick<
-  Repo,
-  "name" | "description" | "forks_count" | "stargazers_count" | "html_url" | "language" | "homepage" | "created_at"
-> & {
-  fromNow: Repo["created_at"];
-} & selectedProjectType;
+export type repoType = Pick<"id" & cardRepoType>;
+
+export type fullCardRepoType = cardRepoType & selectedProjectType;
 
 export type coverType = Pick<File, "relativePath"> & {
   childImageSharp?: Maybe<Pick<ImageSharp, "gatsbyImageData">> | undefined;
