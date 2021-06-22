d6cadd5f4c5e63db56833ec6e30ec997032364d8
MedElBoudali <med.sneaky@gmail.com>
2021-06-22 22:46:09 +0000

    💄 + TechnologiesWrapper.

--- src/components/common/card/Technologies.tsx
+++ src/components/common/card/Technologies.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { project_topics_type } from "../../../types/projects";
 import TechIcon from "./TechIcon";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+
+const TechnologiesWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  flex-wrap: wrap;
+  margin: 20px 0;
+  height: 48px;
+`;
 
 interface TechnologiesProps {
   project_topis: project_topics_type[];
@@ -9,11 +20,11 @@ interface TechnologiesProps {
 
 const Technologies = ({ project_topis }: TechnologiesProps) => {
   return (
-    <div>
+    <TechnologiesWrapper>
       {project_topis.map((topic, i) => (
         <TechIcon key={i} tech={topic} />
       ))}
-    </div>
+    </TechnologiesWrapper>
   );
 };
 
