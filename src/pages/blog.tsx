ae3f3608ea115ee26bb04e0eb58d87765bd6030d
MedElBoudali <med.sneaky@gmail.com>
2021-07-02 23:43:27 +0000

    ✨ + postsQuery.

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,54 @@
 import React from "react";
+import { graphql, Link } from "gatsby";
+import { AllPostsQuery } from "../../gatsby-graphql";
+import PageTitle from "../components/common/PageTitle";
 import PropTypes from "prop-types";
 
-interface BlogProps {}
+interface BlogProps {
+  data: AllPostsQuery;
+}
 
-const Blog = ({}: BlogProps) => {
+const Blog = ({
+  data: {
+    allMdx: { nodes: MDX, totalCount },
+  },
+}: BlogProps) => {
   // FIXME: fix this page later
-  return <div>This is blog Component/Page</div>;
+  return (
+    <div>
+      <PageTitle ItemsCountNumber={totalCount}>blog</PageTitle>
+      This is blog Component/Page
+      {MDX.map(mdx => (
+        <>
+          <p key={mdx.id}>{mdx.frontmatter?.title}</p>
+          <Link to={`${mdx.frontmatter?.slug!}`}>Click Me</Link>
+        </>
+      ))}
+    </div>
+  );
 };
 
 Blog.propTypes = {};
 
 export default Blog;
+
+export const query = graphql`
+  query allPosts {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          summary
+          cover
+          author
+          tags
+          slug
+          date(formatString: "YYYY MMMM Do")
+        }
+      }
+      totalCount
+    }
+  }
+`;
