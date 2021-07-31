5eb4b419cefbb7dc3edea291140864ef2d3083d8
MedElBoudali <med.sneaky@gmail.com>
2021-07-31 12:39:19 +0000

    💄 + svg > path style, - return statement.

--- src/components/about/Button.tsx
+++ src/components/about/Button.tsx
@@ -19,25 +19,23 @@ const ButtonWrapper = styled.a`
     letter-spacing: -0.05em;
     text-transform: uppercase;
   }
+  svg > path {
+    fill: var(--white);
+  }
 `;
 
 interface ButtonProps {
   url: string;
 }
 
-const Button = ({ url }: ButtonProps) => {
-  return (
-    <ButtonWrapper href={url} aria-label="resume" rel="noreferrer" download>
-      <span>resume</span>
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M9 4.25V0H2.75C2.33437 0 2 0.334375 2 0.75V15.25C2 15.6656 2.33437 16 2.75 16H13.25C13.6656 16 14 15.6656 14 15.25V5H9.75C9.3375 5 9 4.6625 9 4.25ZM11.3891 10.855L8.37594 13.8456C8.16812 14.0522 7.8325 14.0522 7.62469 13.8456L4.61156 10.855C4.29438 10.5403 4.51687 10 4.96312 10H7V7.5C7 7.22375 7.22375 7 7.5 7H8.5C8.77625 7 9 7.22375 9 7.5V10H11.0369C11.4831 10 11.7056 10.5403 11.3891 10.855ZM13.7812 3.28125L10.7219 0.21875C10.5813 0.078125 10.3906 0 10.1906 0H10V4H14V3.80938C14 3.6125 13.9219 3.42188 13.7812 3.28125Z"
-          fill="white"
-        />
-      </svg>
-    </ButtonWrapper>
-  );
-};
+const Button = ({ url }: ButtonProps) => (
+  <ButtonWrapper href={url} aria-label="resume" rel="noreferrer" download>
+    <span>resume</span>
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 4.25V0H2.75C2.33437 0 2 0.334375 2 0.75V15.25C2 15.6656 2.33437 16 2.75 16H13.25C13.6656 16 14 15.6656 14 15.25V5H9.75C9.3375 5 9 4.6625 9 4.25ZM11.3891 10.855L8.37594 13.8456C8.16812 14.0522 7.8325 14.0522 7.62469 13.8456L4.61156 10.855C4.29438 10.5403 4.51687 10 4.96312 10H7V7.5C7 7.22375 7.22375 7 7.5 7H8.5C8.77625 7 9 7.22375 9 7.5V10H11.0369C11.4831 10 11.7056 10.5403 11.3891 10.855ZM13.7812 3.28125L10.7219 0.21875C10.5813 0.078125 10.3906 0 10.1906 0H10V4H14V3.80938C14 3.6125 13.9219 3.42188 13.7812 3.28125Z" />
+    </svg>
+  </ButtonWrapper>
+);
 
 Button.propTypes = { url: PropTypes.string.isRequired };
 
