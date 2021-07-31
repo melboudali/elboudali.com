f569fb5aa0808c7124abadb67f0cf33579bf04e1
MedElBoudali <med.sneaky@gmail.com>
2021-07-31 20:46:54 +0000

    ♻️ -+ file name (MoreProjectsLink -> MoreProjects).

---
+++ src/components/projects/MoreProjects.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import about from "../../data/about";
+
+const MoreProjectsWrapper = styled.div`
+  --width: 100%;
+  width: var(--width);
+  height: 50px;
+  margin: 50px auto;
+  background-color: ${({ theme }) => theme.selectBackground};
+  border-bottom-left-radius: 5px;
+  border-top-right-radius: 5px;
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    color: var(--white);
+    font-weight: bold;
+    font-size: 1.25rem;
+    letter-spacing: -0.04em;
+    text-transform: uppercase;
+  }
+  @media (min-width: 500px) {
+    --width: 370px;
+  }
+`;
+
+const MoreProjects = () => (
+  <MoreProjectsWrapper>
+    <a href={`${about.socialLinks.github}?tab=repositories`} target="_blank">
+      more projects
+    </a>
+  </MoreProjectsWrapper>
+);
+
+export default MoreProjects;
