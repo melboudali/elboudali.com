0a247ab3eb7b4f9975efc625a52f5e6bf417c211
MedElBoudali <med.sneaky@gmail.com>
2021-08-02 22:03:06 +0000

    ✨ -+ error page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,51 @@
 import React from "react";
-import PropTypes from "prop-types";
+import styled from "styled-components";
 
-interface ErrorPageProps {}
+const PageWrapper = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
-const ErrorPage = ({}: ErrorPageProps) => (
-  <>
-    // FIXME: fix this page later
-    <h1>Not Found Page</h1>
-  </>
+const ErrorWrapper = styled.h1`
+  font-size: 15rem;
+  font-weight: 900;
+  color: ${({ theme }) => theme.errorColor};
+  letter-spacing: 10px;
+  margin: 0;
+`;
+
+const ErrorNumber = styled.h2`
+  font-size: 8rem;
+  font-weight: 900;
+  color: ${({ theme }) => theme.errorNumberColor};
+  letter-spacing: 10px;
+  margin: 0;
+  margin-top: -8rem;
+`;
+
+const ErrorTitle = styled.h2`
+  color: ${({ theme }) => theme.errorNumberColor};
+  text-transform: capitalize;
+  font-size: 3rem;
+  margin: 0 0 30px 0;
+`;
+
+const ErrorMessage = styled.p`
+  color: ${({ theme }) => theme.errorNumberColor};
+  font-weight: 600;
+  margin: 0;
+  &:first-letter {
+    text-transform: uppercase;
+  }
+`;
+
+export default () => (
+  <PageWrapper>
+    <ErrorWrapper>error</ErrorWrapper>
+    <ErrorNumber>404</ErrorNumber>
+    <ErrorTitle>page not found</ErrorTitle>
+    <ErrorMessage>the page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</ErrorMessage>
+  </PageWrapper>
 );
-
-ErrorPage.propTypes = {};
-
-export default ErrorPage;
