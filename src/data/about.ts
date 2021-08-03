e7b5bbaff8cae5ff23483144369f7d9b11d6af00
MedElBoudali <med.sneaky@gmail.com>
2021-08-03 19:30:39 +0000

    📝 -+ data modified.

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,10 +1,10 @@
 const about = {
   fullName: "mohamed el boudali",
   summary: {
-    short: ["I", "❤", "coding w/ JavaScript, TypeScript, React, Gatsby, Next, graphQL and Express."],
+    short: ["I", "❤", "coding w/ JavaScript, TypeScript, React, Gatsby, Next, graphQL, and Express."],
     long: [
-      " I'm a self-taught Full Stack Developer, proficient in Front End, with 2+ years of hands-on experience designing, developing, and implementing applications and solutions using a range of technologies.",
-      " Seeking to leverage broad development experience and hands-on technical expertise in a challenging role as a Full Stack or a Front End Developer.",
+      "I'm a Self-Taught Full Stack Developer, proficient in Front End, with 3+ years of hands-on experience designing, developing, and implementing applications and solutions using a range of technologies.",
+      "Seeking to leverage broad development experience and hands-on technical expertise in a challenging role as a Front End or a Full Stack Developer.",
       "I love open source and building side projects using JavaScript tools.",
       "This website is my digital garden — a compendium of the things I've learned and created over the years.",
     ],
@@ -18,31 +18,30 @@ const about = {
     twitter: "https://twitter.com/moelboudali",
     freecodecamp: "https://www.freecodecamp.org/elboudali",
     hackerrank: "https://www.hackerrank.com/med_sneaky",
-    reddit: "https://www.reddit.com/user/mflyback/",
     twitterId: "@moelboudali",
   },
   schoolDegrees: [
     {
-      title: "front end development libraries certification.",
+      title: "front end development libraries certification",
       schoolName: "freecodecamp",
       certificationLink: "https://www.freecodecamp.org/certification/elboudali/front-end-libraries",
     },
     {
-      title: "javaScript algorithms and data structures certification.",
+      title: "javaScript algorithms and data structures certification",
       schoolName: "freecodecamp",
       certificationLink: "https://www.freecodecamp.org/certification/elboudali/javascript-algorithms-and-data-structures",
     },
     {
-      title: "responsive web design certification.",
+      title: "responsive web design certification",
       schoolName: "freecodecamp",
       certificationLink: "https://www.freecodecamp.org/certification/elboudali/responsive-web-design",
     },
     {
-      title: "diploma of specialized technician in computer network techniques.",
+      title: "diploma of specialized technician in computer network techniques",
       schoolName: "ofppt - ista ntic",
       abbrExpantion: "specialized institute of applied technology NTIC",
     },
-    { title: "baccalauréat in experimental science, life and earth.", schoolName: "hassan ii high school" },
+    { title: "baccalauréat in experimental science, life and earth", schoolName: "hassan ii high school" },
   ],
   siteUrl: "https://elboudali.com",
   siteLogo: "< el boudali />",
