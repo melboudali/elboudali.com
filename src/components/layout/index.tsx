5c575c41681d619d1ea18d398e383697444d83c6
MedElBoudali <med.sneaky@gmail.com>
2021-08-10 14:36:39 +0000

    🔍 - Helmet.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -38,9 +38,6 @@ const Layout = ({ children }: LayoutProps) => {
 
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
-      </Helmet>
       <GlobalStyles />
       <Header>
         <Navbar themeToggler={themeToggler} theme={theme} />
