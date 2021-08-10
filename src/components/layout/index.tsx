66208b1cd71cb359ea6354540d1c50249d50aadb
MedElBoudali <med.sneaky@gmail.com>
2021-08-10 13:44:22 +0000

    🔍 - Seo.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -38,9 +38,6 @@ const Layout = ({ children }: LayoutProps) => {
 
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <Seo>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
-      </Seo>
       <GlobalStyles />
       <Header>
         <Navbar themeToggler={themeToggler} theme={theme} />
