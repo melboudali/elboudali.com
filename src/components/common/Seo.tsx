8baa9ff67ac77919661aba5b1589470e5ec94c6a
MedElBoudali <med.sneaky@gmail.com>
2021-08-11 10:38:57 +0000

    🔍 + meta tags and defaultProps.

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -8,10 +8,12 @@ interface SeoProps {
   title?: string;
   image?: string;
   description?: string;
+  children?: React.ReactNode;
   location?: string;
+  type: "website" | "article";
 }
 
-const Seo = ({ title, image, description, location }: SeoProps) => {
+const Seo = ({ title, image, description, children, location, type }: SeoProps) => {
   const { site }: MetaDataQuery = useStaticQuery(graphql`
     query metaData {
       site {
@@ -35,20 +37,29 @@ const Seo = ({ title, image, description, location }: SeoProps) => {
 
   return (
     <Helmet title={seo.title!}>
+      <html lang="en" />
+
       <meta name="description" content={seo.description!} />
       {seo.image && <meta name="image" content={seo.image} />}
 
       <meta property="og:url" content={seo.siteUrl!} />
-      <meta property="og:type" content="article" />
+      <meta property="og:type" content={type} />
       <meta property="og:title" content={seo.title!} />
       <meta property="og:description" content={seo.description!} />
       {image && <meta property="og:image" content={seo.image} />}
+      <meta
+        property="fb:app_id"
+        content="3820447614661427
+"
+      />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={site?.siteMetadata?.twitter!} />
       <meta name="twitter:title" content={seo.title!} />
       <meta name="twitter:description" content={seo.description!} />
       {image && <meta name="twitter:image" content={seo.image} />}
+      {children}
+      <link rel="canonical" href={seo.siteUrl!} />
     </Helmet>
   );
 };
@@ -57,7 +68,13 @@ Seo.propTypes = {
   title: PropTypes.string,
   image: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   description: PropTypes.string,
+  children: PropTypes.node,
   location: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
+
+Seo.defaultProps = {
+  type: "website",
 };
 
 export default Seo;
