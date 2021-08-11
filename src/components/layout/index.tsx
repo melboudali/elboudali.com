65b2ed82a4993063e5447baae3bcae11ea323ee9
MedElBoudali <med.sneaky@gmail.com>
2021-08-11 11:21:24 +0000

    🔍 -+ Seo.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -36,18 +36,13 @@ const Layout = ({ children }: LayoutProps) => {
   }
 
   return (
-    <>
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
-      </Helmet>
-      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-        <GlobalStyles />
-        <Header>
-          <Navbar themeToggler={themeToggler} theme={theme} />
-        </Header>
-        <Main>{children}</Main>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Header>
+        <Navbar themeToggler={themeToggler} theme={theme} />
+      </Header>
+      <Main>{children}</Main>
+    </ThemeProvider>
   );
 };
 
