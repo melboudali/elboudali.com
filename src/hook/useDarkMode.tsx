139c08ccc4aa083f468eaaa62a485fd84247da34
MedElBoudali <med.sneaky@gmail.com>
2021-08-11 11:34:29 +0000

    🔍 -+ Seo.

--- src/hook/useDarkMode.tsx
+++ src/hook/useDarkMode.tsx
@@ -9,19 +9,23 @@ export const useDarkMode = () => {
     setTheme(mode);
   };
 
-  const themeToggler = () =>
-    theme === "light" ? setMode("dark") : setMode("light");
+  const themeToggler = () => (theme === "light" ? setMode("dark") : setMode("light"));
+
+  // useEffect(() => {
+  //   const localTheme = window.localStorage.getItem("theme");
+  //   window.matchMedia &&
+  //   window.matchMedia("(prefers-color-scheme: dark)").matches &&
+  //   !localTheme
+  //     ? setMode("dark")
+  //     : localTheme
+  //     ? setTheme(localTheme)
+  //     : setMode("light");
+  //   setComponentMounted(true);
+  // }, []);
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme");
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches &&
-    !localTheme
-      ? setMode("dark")
-      : localTheme
-      ? setTheme(localTheme)
-      : setMode("light");
-    setComponentMounted(true);
+    localTheme ? setTheme(localTheme) : setMode("light");
   }, []);
 
   return { theme, themeToggler, componentMounted };
