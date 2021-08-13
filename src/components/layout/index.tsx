9cac1b369626536fb54230fa15e1089dad4045ea
MedElBoudali <med.sneaky@gmail.com>
2021-08-13 21:48:42 +0000

    ✨ + useNavber hook + style.

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,19 +6,25 @@ import { lightTheme } from "../../themes/light";
 import { darkTheme } from "../../themes/dark";
 import Navbar from "../common/navbar";
 import PropTypes from "prop-types";
+import useNavbar from "../../hook/useNavbar";
 
-const Header = styled.header`
+const Header = styled.header<{ showNav: boolean; scrollDown: boolean }>`
+  position: fixed;
+  width: 100%;
+  z-index: 2;
   background-color: ${({ theme }) => theme.navbarBackground};
   box-shadow: 0px 3px 8px -2px var(--navbarBoxShadow);
+  transition: all 0.3s;
+  transform: ${({ showNav, scrollDown }) => !showNav && (scrollDown ? "translate3d(0, 0, 0)" : "translate3d(0, -47px, 0)")};
 `;
 
 const Main = styled.main`
-  --mainPadding: 0 11px;
+  --mainPadding: 94px 11px 50px;
   max-width: 1280px;
-  margin: 50px auto;
+  margin: 0 auto;
   padding: var(--mainPadding);
   @media (min-width: 750px) {
-    --mainPadding: 0 48px;
+    --mainPadding: 94px 48px 50px;
   }
 `;
 
@@ -28,11 +34,11 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   const { theme, themeToggler } = useDarkMode();
-
+  const { showNav, scrollDown } = useNavbar(47);
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyles />
-      <Header>
+      <Header showNav={showNav} scrollDown={scrollDown}>
         <Navbar themeToggler={themeToggler} theme={theme} />
       </Header>
       <Main>{children}</Main>
