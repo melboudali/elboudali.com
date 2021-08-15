6fc5a4e0f9b4e21104b8d10b12f9f6cbf873c3c1
MedElBoudali <med.sneaky@gmail.com>
2021-08-15 13:21:17 +0000

    ✨ + config file.

---
+++ src/data/config.ts
@@ -0,0 +1,15 @@
+import about from "./about";
+
+const config = {
+  title: `${about.fullName} | ${about.role}`,
+  siteUrl: "https://elboudali.com",
+  siteLogo: "< el boudali />",
+  featuredImage: "/images/thumbnail.png",
+  backgroundColor: `#191820`,
+  themeColor: `#16141F`,
+  icon: `src/assets/images/icon.png`,
+  fbid: "3820447614661427",
+  googleAnalyticsTrackingID: "G-R5X3QX1HXG",
+};
+
+export default config;
