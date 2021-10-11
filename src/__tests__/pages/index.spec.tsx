72b03e47f254cd378541f41cf65b49dcd50a95d1
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-11 23:14:23 +0100

    :test_tube: + it should check certifs links

--- src/__tests__/pages/index.spec.tsx
+++ src/__tests__/pages/index.spec.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
 import Index from "../../pages/index";
 import * as Gatsby from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 beforeEach(() => {
   const useStaticQuery = jest.spyOn(Gatsby, "useStaticQuery");
@@ -13,7 +14,7 @@ afterEach(() => {
 });
 
 describe("Testing Index Page", () => {
-  it("render social links", () => {
+  it("should render social links", () => {
     render(<Index />);
     expect(screen.getByTitle("Email")).toHaveAttribute("href", "mailto:contact@elboudali.com");
     expect(screen.getByTitle("Linkedin")).toHaveAttribute("href", "https://www.linkedin.com/in/elboudali");
@@ -24,5 +25,21 @@ describe("Testing Index Page", () => {
       "https://www.freecodecamp.org/elboudali"
     );
     expect(screen.getByTitle("Hackerrank")).toHaveAttribute("href", "https://www.hackerrank.com/med_sneaky");
+  });
+
+  it("should check certifs links", () => {
+    render(<Index />);
+    expect(screen.getAllByRole("link", { name: "certification" })[0]).toHaveAttribute(
+      "href",
+      "https://www.freecodecamp.org/certification/elboudali/front-end-libraries"
+    );
+    expect(screen.getAllByRole("link", { name: "certification" })[1]).toHaveAttribute(
+      "href",
+      "https://www.freecodecamp.org/certification/elboudali/javascript-algorithms-and-data-structures"
+    );
+    expect(screen.getAllByRole("link", { name: "certification" })[2]).toHaveAttribute(
+      "href",
+      "https://www.freecodecamp.org/certification/elboudali/responsive-web-design"
+    );
   });
 });
