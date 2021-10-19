314334847444371348133cb1e7782cb4ad826b79
Moe EL BOUDALI <med.sneaky@gmail.com>
2021-10-19 19:16:02 +0100

    :label: + AllMdxQueryType and NodeType

--- src/types/blog.d.ts
+++ src/types/blog.d.ts
@@ -1,8 +1,17 @@
-import { Maybe, Mdx, MdxFrontmatter } from "../../gatsby-graphql";
+import { mdx } from "@mdx-js/react";
+import { Maybe, Mdx, MdxFrontmatter, MetaDataQuery } from "../../gatsby-graphql";
 
 export type MDXType = Pick<Mdx, "id" | "timeToRead" | "excerpt"> & {
   fields?: Maybe<Pick<MdxFields, "slug">>;
   frontmatter?: Maybe<
-    Pick<MdxFrontmatter, "title" | "summary" | "tags" | "date"> & { cover?: Maybe<{ childImageSharp?: Maybe<Pick<ImageSharp, "gatsbyImageData">> }> }
+    Pick<MdxFrontmatter, "title" | "summary" | "tags" | "date"> & {
+      cover?: Maybe<{ childImageSharp?: Maybe<Pick<ImageSharp, "gatsbyImageData">> }>;
+    }
   >;
 };
+
+export interface AllMdxQueryType {
+  query: { allMdx: { nodes: Mdx[] } };
+}
+
+export type NodeType = Mdx;
